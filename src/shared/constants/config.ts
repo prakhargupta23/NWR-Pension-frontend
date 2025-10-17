6b93adf4f89c5e76e3bb4833cda9d5a85c8cc2d7
Prakhar Gupta <136190337+prakhargupta23@users.noreply.github.com>
2025-10-17 06:22:16 -0700

    Update API URL in development configuration

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -2,7 +2,7 @@
 
 const dev = Object.freeze({
   // apiUrl: "https://nwr-pension-2025.azurewebsites.net",
-  apiUrl: "https://nwr-portal-fab5b4dzdedafqbx.centralindia-01.azurewebsites.net",
+  apiUrl: "https://railwaysbackend-d9fycvhce4gvhugv.centralindia-01.azurewebsites.net",
   //apiUrl: "http://localhost:7071",
 });
 
