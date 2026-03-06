0f4c6c092a9ca4cf452647de9e0a45ff7f34234b
Prakhar Gupta <136190337+prakhargupta23@users.noreply.github.com>
2026-03-06 16:55:32 +0530

    Update config.ts

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -1,8 +1,8 @@
 /** @format */
 
 const dev = Object.freeze({
-  //apiUrl: "https://backendnwr.azurewebsites.net",
-  apiUrl: "http://localhost:7071",
+  apiUrl: "https://backendnwr.azurewebsites.net",
+  //apiUrl: "http://localhost:7071",
 });
 // export const config = process.env.NODE_ENV === "development" ? dev : prod;
 export const config = dev;
